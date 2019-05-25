6395ea2892bdfb369fff7605da6a36a47c6dc664
Fernando Oliveira <nandooliveira.al@gmail.com>
2019-05-25 10:24:18 -0300

    cbv

--- forums/urls.py
+++ forums/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('<forum_id>', views.show, name="show"),
+    path('topics/', views.TopicListView.as_view(), name="topics")
 ]
--- forums/views.py
+++ forums/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from .models import Forum
+from django.http import HttpResponse, HttpResponseNotFound
+from django.views.decorators.http import require_http_methods
+from django.views.generic.list import ListView
 
+from .models import Forum, Topic
+
+@require_http_methods(['GET'])
 def index(request):
     forums = Forum.objects.all()
     context = { 'forums': forums }
@@ -8,6 +13,14 @@ def index(request):
 
 
 def show(request, forum_id):
-    forum = Forum.objects.get(pk=forum_id)
-    context = { 'forum': forum }
-    return render(request, 'forums/show.html', context)
+        forum = Forum.objects.filter(pk=forum_id).first()
+        if not forum:
+                #return HttpResponseNotFound('Forum não encontrado!')
+                return HttpResponse('Forum não encontrado!', status=404)
+
+        context = { 'forum': forum }
+        return render(request, 'forums/show.html', context)
+    
+
+class TopicListView(ListView):
+    model = Topic
