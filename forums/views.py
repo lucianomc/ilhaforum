06bfa2df7259e7c2e42df9d1be6bc0929a5cddff
Fernando Oliveira <nandooliveira.al@gmail.com>
2019-05-25 08:48:50 -0300

    New views

--- forums/views.py
+++ forums/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Forum
 
-# Create your views here.
+def index(request):
+    forums = Forum.objects.all()
+    context = { 'forums': forums }
+    return render(request, 'forums/index.html', context)
+
+
+def show(request, forum_id):
+    forum = Forum.objects.get(pk=forum_id)
+    context = { 'forum': forum }
+    return render(request, 'forums/show.html', context)
