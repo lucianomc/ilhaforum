e0276a47a07f904ffd1677427cd4c780dc8153ea
Fernando Oliveira <nandooliveira.al@gmail.com>
2019-06-08 09:03:32 -0300

    test

--- forums/urls.py
+++ forums/urls.py
@@ -6,8 +6,12 @@ app_name = 'forums'
 urlpatterns = [
     path('', views.index, name="index"),
     path('<forum_id>', views.show, name="show"),
+    path('forum_json/', views.ForumJsonView.as_view()),
+    path('test_methods/', views.test_methods, name="test_methods"),
+    path('test_methods2/', views.TestMethodsView.as_view(), name="test_methods2"),
     path('topics/', views.TopicListView.as_view(), name="topics"),
     path('topics/<int:pk>', views.TopicDetailView.as_view(), name='topic_details'),
     path('topics/create', views.TopicCreateView.as_view(), name='topic_create'),
-    path('topics/<int:pk>/update', views.TopicUpdateView.as_view(), name='topic_update')
+    path('topics/<int:pk>/update', views.TopicUpdateView.as_view(), name='topic_update'),
+    path('file_upload/', views.FileUploadView.as_view())
 ]
